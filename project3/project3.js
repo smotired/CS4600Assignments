b3af62287a6288dab52d167bf4092e31874db5a8
Sam Hill <soshjam@soshjam.com>
2025-01-19 16:21:44 -0700

    Pass information to vertex shader and create buffers

--- project3/project3.js
+++ project3/project3.js
@@ -6,21 +6,63 @@ class CurveDrawer {
 		this.prog   = InitShaderProgram( curvesVS, curvesFS );
 		// [TO-DO] Other initializations should be done here.
 		// [TO-DO] This is a good place to get the locations of attributes and uniform variables.
+
+		gl.useProgram( this.prog );
+
+		// Get uniform and attribute locations
+
+		// Points
+		this.pt = [];
+		this.pt.push( gl.getUniformLocation(this.prog, "p0") );
+		this.pt.push( gl.getUniformLocation(this.prog, "p1") );
+		this.pt.push( gl.getUniformLocation(this.prog, "p2") );
+		this.pt.push( gl.getUniformLocation(this.prog, "p3") );
+
+		// Transformation matrix
+		this.mvp = gl.getUniformLocation(this.prog, "mvp");
+
+		// t attribute
+		this.t = gl.getAttribLocation(this.prog, "t");
 		
 		// Initialize the attribute buffer
 		this.steps = 100;
-		var tv = [];
+		var tv = []; // t values
 		for ( var i=0; i<this.steps; ++i ) {
 			tv.push( i / (this.steps-1) );
 		}
+
 		// [TO-DO] This is where you can create and set the contents of the vertex buffer object
 		// for the vertex attribute we need.
+
+		// we will represent it as lines i guess so each point appears twice except first and last
+		const lines = [ tv[0] ];
+		for (let i = 1; i < tv.length - 1; i++) {
+			lines.push(tv[i]);
+			lines.push(tv[i]);
+		}
+		lines.push(tv[-1]);
+		this.lineCount = lines.length / 2; // lines.length is the number of vertices and we have 2 lines per vertex
+
+		// Set up the vertex buffer
+		this.vertexBuffer = gl.createBuffer();
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer);
+		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(lines), gl.STATIC_DRAW);
 	}
 	setViewport( width, height )
 	{
 		// [TO-DO] This is where we should set the transformation matrix.
 		// [TO-DO] Do not forget to bind the program before you set a uniform variable value.
 		gl.useProgram( this.prog );	// Bind the program
+
+		// the transformation matrix was taken from lineDrawer
+		const matrix = [ // column major order
+			 2/width,         0, 0, 0,
+			 0,       -2/height, 0, 0,
+			 0,       0,         1, 0,
+			-1,       1,         0, 1,
+		]; // just scales by 2/width and 2/height and translates by -1, 1
+
+		gl.uniformMatrix4fv(this.mvp, false, matrix);
 	}
 	updatePoints( pt )
 	{
@@ -29,11 +71,39 @@ class CurveDrawer {
 		// [TO-DO] We can access the x and y coordinates of the i^th control points using
 		// var x = pt[i].getAttribute("cx");
 		// var y = pt[i].getAttribute("cy");
+		
+		// Bind the program
+		gl.useProgram(this.prog);
+
+		// Set each position
+		for (let i = 0; i < 4; i++) {
+			const x = pt[i].getAttribute("cx");
+			const y = pt[i].getAttribute("cy");
+			gl.uniform2f( this.pt[i], x, y );
+		}
 	}
 	draw()
 	{
 		// [TO-DO] This is where we give the command to draw the curve.
 		// [TO-DO] Do not forget to bind the program and set the vertex attribute.
+
+		// Bind the program
+		gl.useProgram(this.prog);
+
+		// Set the t attribute
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer);
+		gl.vertexAttribPointer(
+			this.t,
+			2,
+			gl.FLOAT,
+			false,
+			0,
+			0,
+		);
+		gl.enableVertexAttribArray(this.t)
+
+		// Draw the lines
+		gl.drawArrays(gl.LINES, 0, this.lineCount);
 	}
 }
 
@@ -73,7 +143,7 @@ var curvesVS = `
 		vec2 final;
 		lerpv2(pb0, pb1, t, final);
 
-		gl_Position = vec4(final,0,1);
+		gl_Position = mvp * vec4(final,0,1);
 	}
 `;
 
